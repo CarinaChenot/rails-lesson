bc5047324a0ffb9b1a1bdd0e984dd5f7b661f900
Carina Chenot <mllecarinach@gmail.com>
2018-01-19 17:32:01 +0100

    add models

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,9 @@
 class Job < ApplicationRecord
+
+    belongs_to :user
+
+    validates :title, :description, presence: true
+    validates :title, length: { maximum: 150 }
+
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,16 @@ class User < ApplicationRecord
   validates :username, :email, presence: true, uniqueness: true
   validates :username, length: { maximum: 150 }
 
+  # before_validation :fill_email
+
+  ## Callbacks rails (comme des hooks)
+  # before_save
+  # after_save
+
+  # private
+
+  # def fill_email
+  #   self.email = 'toto@toto.com'
+  # end
+
 end
